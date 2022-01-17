5bf991b72bffa6a506f5c293ca8554611a7fd1c2
odilektasli <61044813+odilektasli@users.noreply.github.com>
2022-01-17 18:14:13 +0300

    All is well.

--- Source/IT_LAB/SavedGamesHandler.cpp
+++ Source/IT_LAB/SavedGamesHandler.cpp
@@ -2,19 +2,30 @@
 
 
 #include "SavedGamesHandler.h"
+#include "Misc/App.h"
 
 TArray<FString> USavedGamesHandler::GetSavedGamesList()
 {
 	TArray<FString> Output;
 	Output.Empty();
-	FString Path = FPaths::ProjectDir() + "Saved/SaveGames/*.sav";
+	FString Path;
 	
-	IFileManager::Get().FindFiles(Output, *Path, true, false);
-
 	if (PLATFORM_WINDOWS)
 	{
+		FString AppDataPath = FPlatformProcess::UserSettingsDir();
+		FString ProjectName = FApp::GetProjectName();
+		FString AppDataProjectPath = AppDataPath + ProjectName;
+		Path = AppDataProjectPath + "/Saved/SaveGames/*.sav";
 
 	}
 	
+	else
+	{
+		Path = FPaths::ProjectDir() + "Saved/SaveGames/*.sav";
+
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Path: %s"), (FPlatformProcess::UserSettingsDir()));
+	IFileManager::Get().FindFiles(Output, *Path, true, false);
+
 	return Output;
 }
